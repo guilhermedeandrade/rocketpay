b6a921d2180ecd287f8712ede9c571149dfa2644
Guilherme de Andrade <guilhermedeandrade@protonmail.com>
2021-02-22 13:28:22 +0000

    feat: add WelcomeController

---
+++ lib/rocketpay/numbers.ex
@@ -0,0 +1,20 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, file}) do
+    result =
+      file
+      |> String.replace("\n", "")
+      |> String.split(",")
+      |> Stream.map(&String.to_integer/1)
+      |> Enum.sum()
+
+    {:ok, %{result: result}}
+  end
+
+  defp handle_file({:error, _reason}), do: {:error, %{message: "Invalid file!"}}
+end
